c1f615f8b8b01189459e1a87db0a84df007b81fd
devrahul-2508 <devrahul2508@gmail.com>
2024-09-16 10:16:57 +0530

    added compose dependencies

--- android/app/src/main/kotlin/com/example/flutter_alarm_manager_poc/activity/AlarmActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_alarm_manager_poc/activity/AlarmActivity.kt
@@ -2,11 +2,12 @@ package com.example.flutter_alarm_manager_poc.activity
 
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.flutter_alarm_manager_poc.R
 import com.example.flutter_alarm_manager_poc.utils.turnScreenOnAndKeyguardOff
 import io.flutter.embedding.android.FlutterActivity
 
-class AlarmActivity : FlutterActivity() {
+class AlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
