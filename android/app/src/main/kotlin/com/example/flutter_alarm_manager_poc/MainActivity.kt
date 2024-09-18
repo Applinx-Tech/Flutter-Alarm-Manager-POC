f485dd045ba793d25839a925894b49e57182e7c3
devrahul-2508 <devrahul2508@gmail.com>
2024-09-18 12:01:25 +0530

    bug fixes and used cached flutter engine management

--- android/app/src/main/kotlin/com/example/flutter_alarm_manager_poc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_alarm_manager_poc/MainActivity.kt
@@ -6,10 +6,12 @@ import com.example.flutter_alarm_manager_poc.alarmScheduler.AlarmSchedulerImpl
 import com.example.flutter_alarm_manager_poc.model.AlarmItem
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
 
+    private val ENGINE_ID="alarm_manager_engine"
     private val CHANNEL = "com.example/alarm_manager"
     private val TAG = "POC"
 
@@ -18,6 +20,8 @@ class MainActivity : FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+
+        FlutterEngineCache.getInstance().put(ENGINE_ID,flutterEngine)
 
         alarmScheduler = AlarmSchedulerImpl(this)
 
--- android/app/src/main/kotlin/com/example/flutter_alarm_manager_poc/activity/AlarmActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_alarm_manager_poc/activity/AlarmActivity.kt
@@ -1,29 +1,46 @@
 package com.example.flutter_alarm_manager_poc.activity
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
-
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import com.example.flutter_alarm_manager_poc.screens.AlarmScreen
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
 import io.flutter.plugin.common.MethodChannel
 
+
 class AlarmActivity : ComponentActivity() {
+    private val ENGINE_ID = "alarm_manager_engine"
     private val CHANNEL = "com.example/alarm_manager"
+    private var flutterEngine: FlutterEngine? = null
+    private var isNewEngineCreated = false // create new engine when app is closed and use existing when app is resumed state
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         actionBar?.hide()
 
-        val flutterEngine = FlutterEngine(this)
-        flutterEngine.dartExecutor.executeDartEntrypoint(
-            DartExecutor.DartEntrypoint.createDefault()
-        )
+        // Check if a cached engine is available
+        flutterEngine = FlutterEngineCache.getInstance().get(ENGINE_ID)
 
-        val channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+        if (flutterEngine == null) {
+            // If no cached engine is found (app was killed), create a new one
+            flutterEngine = FlutterEngine(this).apply {
+                dartExecutor.executeDartEntrypoint(
+                    DartExecutor.DartEntrypoint.createDefault()
+                )
+            }
+            // Optionally, cache this new engine if needed later
+            FlutterEngineCache.getInstance().put(ENGINE_ID, flutterEngine)
+            isNewEngineCreated = true;
+        }
+
+        // Set up the MethodChannel
+        val channel = MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL)
+
+        // Set the content of the AlarmActivity using Jetpack Compose
         setContent {
             MaterialTheme {
                 Surface(color = MaterialTheme.colorScheme.onSurface) {
@@ -39,6 +56,17 @@ class AlarmActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Only destroy the engine when the notification activity is launched from killed state
+        // Do not kill the engine when the app is in running state otherwise it will lead to multiple flutter main-call stacks.
+        if (isNewEngineCreated) {
+            flutterEngine?.destroy()
+            FlutterEngineCache.getInstance().remove(ENGINE_ID)
         }
     }
 }
